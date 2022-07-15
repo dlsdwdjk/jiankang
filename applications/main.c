3497fdf5ca56bf79268f52af3139eb9b7d6b29ec
dlsdwdjk <108870772+dlsdwdjk@users.noreply.github.com>
2022-07-15 15:15:03 +0800

    Update main.c

--- applications/main.c
+++ applications/main.c
@@ -1,389 +1,278 @@
+#include "ds18b20.h"
+#include "drv_common.h"
+#include "rtdef.h"
+#include "rtdbg.h"
+#include "stm32f1xx.h"
+
 /*
-file: test_user_i2c.c
-测试祼机i2c2驱动程序ls1c_i2c.c 在finsh 中运行
-1. test_at24(1,17) //使用i2c1 , 写入at24c32(地址0~31)数据(17*i(i=0~31)),再读出并打印
-   test_at2402(1,17) //使用i2c1 , 写入at24c02(地址0~7)数据(17*i(i=0~7)),再读出并打印
-2. ds3231_getdata(1) //使用i2c1,读出ds3231的日期
-3. ds3231_gettime(1)  //使用i2c1,读出ds3231的时间
-4. ds3231_setdata(1,180101)  //使用i2c1,写入ds3231的日期(2018.1.1)
-5. ds3231_settime(1,140000)  //使用i2c1,写入ds3231的时间(14：00：00)
+ * 函数名：DS18B20_Mode_Out_PP
+ * 描述  ：使DS18B20-DATA引脚变为输出模式
+ * 输入  ：无
+ * 输出  ：无
  */
-#include <rtthread.h>  
-#include <stdlib.h>  
-#include "ls1c.h"  
-#include "ls1c_public.h"  
-#include "ls1c_i2c.h"  
-#include "ls1c_pin.h"  
-  
-// I2C1
-#define LS1C_I2C_SDA1            (2)  
-#define LS1C_I2C_SCL1            (3)  
-  
-// I2C2 
-#define LS1C_I2C_SDA2          (54)  
-#define LS1C_I2C_SCL2          (55)  
-  
-// 测试at24C32  页面长度32 字节 ，地址2 字节
-void test_at24(rt_int8_t ic_no, rt_int8_t num )    
-{  
-    int i;
-    ls1c_i2c_info_t i2c_info;  
-    int slave_addr = 0xAE >> 1;    
-    unsigned char send_buff[64] = {0};  
-    unsigned char recv_buff[64] = {0};  
-      
-    i2c_info.clock = 50*1000;       // 50kb/s  
-    switch( ic_no)
-    {
-      case 0:
-        i2c_info.I2Cx = LS1C_I2C_0;
-      break;
-      case 1:
-          i2c_info.I2Cx = LS1C_I2C_1;
-      break;
-      case 2:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-      default:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-    }
-    i2c_init(&i2c_info);  
-      
-    send_buff[0] = 0x00;
-    send_buff[1] = 0x00;            
-    for(i=0;i<32;i++)
-    {
-        send_buff[i+2] = i*num;
-    }
-    
-    // 发送器件地址和要写入的地址、数据
-    i2c_send_start_and_addr(&i2c_info, slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 34);  
-    rt_thread_delay(1);  
-    i2c_send_stop(&i2c_info);  
+static void DS18B20_Mode_Out_PP(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
 
-    rt_thread_delay(2);  
-    // 发送读指令  的地址
-    i2c_send_start_and_addr(&i2c_info, slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 2);  
-    i2c_send_stop(&i2c_info);  
-
-    // 读取一页寄存器数据  
-    i2c_send_start_and_addr(&i2c_info, slave_addr, LS1C_I2C_DIRECTION_READ);  
-    i2c_receive_ack(&i2c_info); 
-    i2c_receive_data(&i2c_info, recv_buff, 32);  
-    i2c_send_stop(&i2c_info);  
- 
-    for(i=0;i<32;i++)
-    {
-        rt_kprintf("  0x%02x  ",  recv_buff[i] );
-    }
-    rt_kprintf("\n\r");
+     /*选择要控制的macDS18B20_DQ_GPIO_PORT引脚*/
+    GPIO_InitStructure.Pin = GPIO_PIN_13;
+    /*设置引脚模式为通用推挽输出*/
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    /*设置引脚速率为50MHz */
+    GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
+    /*调用库函数，初始化macDS18B20_DQ_GPIO_PORT*/
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
-// 测试at24C02   I2C总线E2PROM  2KBit  每页8字节  一共256页 ，写的时候注意地址范围是0-255
-void test_at2402(rt_int8_t ic_no, rt_int8_t num )    
-{  
-    int i;
-    ls1c_i2c_info_t i2c_info;  
-    int slave_addr = 0x50;    
-    unsigned char send_buff[64] = {0};  
-    unsigned char recv_buff[64] = {0};  
-      
-    i2c_info.clock = 50*1000;       // 50kb/s  
-    switch( ic_no)
-    {
-      case 0:
-        i2c_info.I2Cx = LS1C_I2C_0;
-      break;
-      case 1:
-          i2c_info.I2Cx = LS1C_I2C_1;
-      break;
-      case 2:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-      default:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-    }
-    i2c_init(&i2c_info);  
-      
-    send_buff[0] = 0x00;
-    for(i=0;i<8;i++)
-    {
-        send_buff[i+1] = i*num;
-    }
-    
-    // 发送器件地址和要写入的地址、数据
-    i2c_send_start_and_addr(&i2c_info, slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 9);  
-    rt_thread_delay(1);  
-    i2c_send_stop(&i2c_info);  
-	
-    rt_thread_delay(2);  
+/*
+ * 使DS18B20的数据引脚变为输入模式
+ * */
+static void DS18B20_Mode_IPU(void)
+{
+    GPIO_InitTypeDef GPIO_Initure;
 
-    // 发送读指令  的地址
-    i2c_send_start_and_addr(&i2c_info, slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 1);  
-    i2c_send_stop(&i2c_info);  
+    GPIO_Initure.Pin = GPIO_PIN_13;
+    GPIO_Initure.Mode = GPIO_MODE_INPUT; //浮空模式
 
-    // 读取一页寄存器数据  
-    i2c_send_start_and_addr(&i2c_info, slave_addr, LS1C_I2C_DIRECTION_READ);  
-    i2c_receive_ack(&i2c_info); 
-    i2c_receive_data(&i2c_info, recv_buff, 8);  
-    i2c_send_stop(&i2c_info);  
- 
-    for(i=0;i<8;i++)
-    {
-        rt_kprintf("  0x%02x  ",  recv_buff[i] );
-    }
-    rt_kprintf("\n\r");
+    HAL_GPIO_Init(GPIOC,&GPIO_Initure);
 }
 
-/* 测试ds3231   */
-#define ds3231_slave_addr 0x68
-#define bcd(x)  ( ((x/10)<<4) | (x%10))
-#define frombcd(x)   ((x >> 4) * 10 + (x & 0x0f))
+/*
+ * 主机给DS18B20发送复位脉冲
+ * */
+static void DS18B20_Reset(void)
+{
+    /*主机设置为推挽输出*/
+    DS18B20_Mode_Out_PP();
+    /*主机控制产生至少480us的低电平复位信号*/
+    DS18B20_DATA_0;
+    rt_hw_us_delay(750);
+    /*释放总线*/
+    DS18B20_DATA_1;
 
-void ds3231_getdata(rt_int8_t ic_no )    
-{  
-    ls1c_i2c_info_t i2c_info;  
-    unsigned char send_buff[64] = {0};  
-    unsigned char recv_buff[64] = {0};  
-      
-    i2c_info.clock = 50*1000;       // 50kb/s  
-    switch( ic_no)
+    /*设备接收到复位信号后，会等待15-60us，然后开始给主机发送存在脉冲*/
+    rt_hw_us_delay(15);
+}
+
+/*
+ * 检测DS18B20存在脉冲
+ * return: 0 成功检测设备存在
+ *        -1 检测设备存在失败
+ * */
+static uint8_t DS18B20_Check(void)
+{
+    uint8_t pulse_time = 0;
+
+    /*设置为上拉输入*/
+    DS18B20_Mode_IPU();
+    /*存在脉冲为一个60-240us的低电平信号*/
+    /*如果没有接收到低电平存在信号就一直循环，直到pluse_time达到100*/
+    while(HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_13) && pulse_time<100)
+    {
+        pulse_time++;
+        rt_hw_us_delay(1);
+    }
+
+    /*经过100us还没接收到存在信号*/
+    if(pulse_time >= 100)
+    {
+        rt_kprintf("Receive presence pulse failed!\n");
+        return -1;
+    }
+    else
+    {
+        pulse_time = 0;
+    }
+
+    /*检测到存在信号*/
+    while(!DS18B20_DATA_READ() && pulse_time<240)
+    {
+        pulse_time++;
+        rt_hw_us_delay(1);
+    }
+    if(pulse_time > 240)
+    {
+        rt_kprintf("presense pulse incorrect!\n");
+        return -1;
+    }
+    else
+    {
+        rt_kprintf("Receive presence pulse successful!\n");
+        return 0;
+    }
+}
+
+/*
+ * 写一个字节到DS18B20，低位先行
+ * */
+void DS18B10_WriteByte(uint8_t data)
+{
+    uint8_t i;
+    uint8_t current_bit;
+
+    /*循环发送8位数据*/
+    for(i = 0; i < 8; i++)
+    {
+        current_bit = data & 0x01;    //取当前最低位
+        data = data >> 1;
+
+        /*写1时段，拉低后15us内必须释放*/
+        if(current_bit)
         {
-        case 0:
-        i2c_info.I2Cx = LS1C_I2C_0;
-      break;
-      case 1:
-          i2c_info.I2Cx = LS1C_I2C_1;
-      break;
-      case 2:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-      default:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-}
-    i2c_init(&i2c_info);  
-      
-    send_buff[0] = 0x04;    
-    // 发送器件地址和要写入的地址、数据
-    i2c_send_start_and_addr(&i2c_info, ds3231_slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 1);  
-    rt_thread_delay(1);  
-    i2c_send_stop(&i2c_info);  
-
-    // 读取数据
-    rt_thread_delay(2);  
-    i2c_send_start_and_addr(&i2c_info, ds3231_slave_addr, LS1C_I2C_DIRECTION_READ);  
-    i2c_receive_ack(&i2c_info); 
-    i2c_receive_data(&i2c_info, recv_buff, 3);  
-    i2c_send_stop(&i2c_info);  
-
- 
-    rt_kprintf("\ndata : 20%02d-%02d-%02d\n ",frombcd(recv_buff[2]),frombcd(recv_buff[1]),frombcd(recv_buff[0]));
-    rt_kprintf("\n\r");
-}
-
-void ds3231_gettime(rt_int8_t ic_no )    
-{  
-    ls1c_i2c_info_t i2c_info;  
-    unsigned char send_buff[64] = {0};  
-    unsigned char recv_buff[64] = {0};  
-      
-    i2c_info.clock = 50*1000;       // 50kb/s  
-    switch( ic_no)
+            DS18B20_DATA_0;
+            rt_hw_us_delay(10);
+            DS18B20_DATA_1;
+            rt_hw_us_delay(58);
+         }
+        /*写0时段，拉低后整个时段期间主设备需一直拉低60-120us*/
+        else
         {
-        case 0:
-        i2c_info.I2Cx = LS1C_I2C_0;
-      break;
-      case 1:
-          i2c_info.I2Cx = LS1C_I2C_1;
-      break;
-      case 2:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-      default:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-}
-    i2c_init(&i2c_info);  
-      
-    send_buff[0] = 0x00;    
-    // 发送器件地址和要写入的地址、数据
-    i2c_send_start_and_addr(&i2c_info, ds3231_slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 1);  
-    rt_thread_delay(1);  
-    i2c_send_stop(&i2c_info);  
+            DS18B20_DATA_0;
+            rt_hw_us_delay(70);
+            DS18B20_DATA_1;
 
-    // 读取数据
-    rt_thread_delay(2);  
-    i2c_send_start_and_addr(&i2c_info, ds3231_slave_addr, LS1C_I2C_DIRECTION_READ);  
-    i2c_receive_ack(&i2c_info); 
-    i2c_receive_data(&i2c_info, recv_buff, 3);  
-    i2c_send_stop(&i2c_info);  
+            /* 1us < 恢复时间 < 无穷大 */
+            rt_hw_us_delay(2);
+        }
 
- 
-    rt_kprintf("\ntime : %02d:%02d:%02d\n",frombcd(recv_buff[2]),frombcd(recv_buff[1]),frombcd(recv_buff[0]));
-    rt_kprintf("\n\r");
+    }
+
 }
 
-/*设置日期格式为yymmdd。如2018-01-01写为180101*/
-void ds3231_setdata(rt_int8_t ic_no , rt_uint32_t data)    
-{  
-    ls1c_i2c_info_t i2c_info;  
-    unsigned char send_buff[64] = {0};  
-    unsigned char recv_buff[64] = {0};  
-    rt_uint8_t yy,mm,dd;
-    
-    yy = data / 10000;
-    mm = data % 10000  / 100;
-    dd= data % 100;
-
-    //设置寄存器地址4    
-    send_buff[0]=4;//寄存器地址4
-    send_buff[1]=bcd(dd);//日
-    send_buff[2]=bcd(mm);//月
-    send_buff[3]=bcd(yy);//年
-    
-    i2c_info.clock = 50*1000;       // 50kb/s  
-    switch( ic_no)
-        {
-        case 0:
-        i2c_info.I2Cx = LS1C_I2C_0;
-      break;
-      case 1:
-          i2c_info.I2Cx = LS1C_I2C_1;
-      break;
-      case 2:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-      default:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-}
-    i2c_init(&i2c_info);  
-
-    // 发送器件地址和要写入的地址、数据
-    i2c_send_start_and_addr(&i2c_info, ds3231_slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 4);  
-    rt_thread_delay(1);  
-    i2c_send_stop(&i2c_info);  
-}
-
-/*设置到ds3231。 格式为hhmmss。如14:00:00写为140000*/
-void ds3231_settime(rt_int8_t ic_no , rt_uint32_t time)    
-{  
-    ls1c_i2c_info_t i2c_info;  
-    unsigned char send_buff[64] = {0};  
-    unsigned char recv_buff[64] = {0};  
-    rt_uint8_t hh,mm,ss;
-    
-    hh = time / 10000;
-    mm = time % 10000  / 100;
-    ss = time % 100;
-
-    //设置寄存器地址0    
-    send_buff[0]=0;//寄存器地址0
-    send_buff[1]=bcd(ss);//秒
-    send_buff[2]=bcd(mm);//分
-    send_buff[3]=bcd(hh);//时
-    
-    i2c_info.clock = 50*1000;       // 50kb/s  
-    switch( ic_no)
-        {
-        case 0:
-        i2c_info.I2Cx = LS1C_I2C_0;
-      break;
-      case 1:
-          i2c_info.I2Cx = LS1C_I2C_1;
-      break;
-      case 2:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-      default:
-          i2c_info.I2Cx = LS1C_I2C_2;
-      break;
-}
-    i2c_init(&i2c_info);  
-      
-    
-    // 发送器件地址和要写入的地址、数据
-    i2c_send_start_and_addr(&i2c_info, ds3231_slave_addr, LS1C_I2C_DIRECTION_WRITE);  
-    i2c_receive_ack(&i2c_info);  
-    i2c_send_data(&i2c_info, send_buff, 4);  
-    rt_thread_delay(1);  
-    i2c_send_stop(&i2c_info);  
-}
-
-void test_at24_msh(int argc, char** argv)
+/*
+ * 从DS18B20读取一个bit
+ * */
+uint8_t DS18B20_ReadBit(void)
 {
-    unsigned int num1,num2;
-	num1 = strtoul(argv[1], NULL, 0);
-	num2 = strtoul(argv[2], NULL, 0);
-	test_at24(num1, num2);
-}
-void test_at2402_msh(int argc, char** argv)
-{
-    unsigned int num1,num2;
-	num1 = strtoul(argv[1], NULL, 0);
-	num2 = strtoul(argv[2], NULL, 0);
-	test_at2402(num1, num2);
-}
-void ds3231_getdata_msh(int argc, char** argv)
-{
-    unsigned int num1,num2;
-	num1 = strtoul(argv[1], NULL, 0);
-	ds3231_getdata(num1);
-}
-void ds3231_gettime_msh(int argc, char** argv)
-{
-    unsigned int num1,num2;
-	num1 = strtoul(argv[1], NULL, 0);
-	ds3231_gettime(num1);
-}
-void ds3231_setdata_msh(int argc, char** argv)
-{
-    unsigned int num1,num2;
-	num1 = strtoul(argv[1], NULL, 0);
-	num2 = strtoul(argv[2], NULL, 0);
-	ds3231_setdata(num1, num2);
-}
-void ds3231_settime_msh(int argc, char** argv)
-{
-    unsigned int num1,num2;
-	num1 = strtoul(argv[1], NULL, 0);
-	num2 = strtoul(argv[2], NULL, 0);
-	ds3231_settime(num1, num2);
-}
-int main()
-{
-    return;
+    uint8_t data;
 
+    DS18B20_Mode_Out_PP();
+
+    /*读，需由主机产生1-15us的低电平*/
+    DS18B20_DATA_0;
+    rt_hw_us_delay(10);
+
+    /*开始读bit*/
+    /*设置为输入，释放总线，由外部上拉电阻将总线拉高*/
+    DS18B20_Mode_IPU();
+
+    if(DS18B20_DATA_READ() == 1)
+    {
+        data = 1;
+    }
+    else
+    {
+        data = 0;
+    }
+
+    rt_hw_us_delay(45);
+
+    return data;
 }
- #include  <finsh.h> 
-FINSH_FUNCTION_EXPORT(test_at24 , test_at24  e.g.test_at24(1,17));
-FINSH_FUNCTION_EXPORT(test_at2402 , test_at2402  e.g.test_at2402(2,17));
-FINSH_FUNCTION_EXPORT(ds3231_getdata , ds3231_getdata  e.g.ds3231_getdata(1));
-FINSH_FUNCTION_EXPORT(ds3231_gettime , ds3231_gettime  e.g.ds3231_gettime(1));
-FINSH_FUNCTION_EXPORT(ds3231_setdata , ds3231_setdata  e.g.ds3231_setdata(1,180101));
-FINSH_FUNCTION_EXPORT(ds3231_settime , ds3231_settime  e.g.ds3231_settime(1,140000));
-/* 导出到 msh 命令列表中 */
-MSH_CMD_EXPORT(test_at24_msh, test_at24_msh 1 17);
-MSH_CMD_EXPORT(test_at2402_msh, test_at2402_msh 2 17);
-MSH_CMD_EXPORT(ds3231_getdata_msh, ds3231_getdata_msh 1);
-MSH_CMD_EXPORT(ds3231_gettime_msh, ds3231_gettime_msh 1);
-MSH_CMD_EXPORT(ds3231_setdata_msh, ds3231_setdata_msh 1 180101);
-MSH_CMD_EXPORT(ds3231_settime_msh, ds3231_settime_msh 1 140000);
+
+/*
+ * 从DS18B20读取一个字节
+ * */
+uint8_t DS18B20_ReadByte(void)
+{
+    uint8_t i, j;
+    uint8_t data = 0x00;
+
+    for (i = 0; i<8; i++)
+    {
+        j = DS18B20_DATA_READ();
+        data = data |(j << i);
+    }
+    return data;
+}
+
+/*
+ * 跳过匹配ROM(总线仅一个设备时)
+ * */
+static void DS18B20_SkipRom(void)
+{
+    DS18B20_Reset();
+    DS18B20_Check();
+    DS18B10_WriteByte(0xCC);
+}
+
+/*
+ *开始温度转换
+ * */
+void DS18B20_Start(void)
+{
+    DS18B20_Reset();
+    DS18B20_Check();
+    DS18B10_WriteByte(0xcc);
+    DS18B10_WriteByte(0x44);
+}
+
+
+/*
+ * 读取DS18B20温度值
+ * */
+float DS18B20_GetTemp_SkipRom(void)
+{
+    uint8_t TH,TL;
+    short s_tem;
+    float f_tem;
+
+    DS18B20_SkipRom();
+    DS18B10_WriteByte(0x44);  //温度转换
+
+    DS18B20_SkipRom();
+    DS18B10_WriteByte(0xBE);  //读取温度值
+
+    TL = DS18B20_ReadByte();
+    TH = DS18B20_ReadByte();
+
+    s_tem = TH<<8;
+    s_tem = s_tem|TL;
+
+    if( s_tem < 0 )     /* 负温度 */
+        f_tem = (~s_tem+1) * 0.0625;
+    else
+        f_tem = s_tem * 0.0625;
+
+    return f_tem;
+}
+
+/*
+ * 初始化DS18B20 IO口，同时检测DS18B20的存在
+ * return: 1 初始化失败
+ *         0 初始化成功
+ * */
+uint8_t DS18B20_Init(void)
+{
+
+    GPIO_InitTypeDef GPIO_Initure;
+
+    rt_kprintf("Start to initialize ds18B20\n");
+    /*GPIO初始化*/
+    __HAL_RCC_GPIOC_CLK_ENABLE();         //开启GPIOC时钟
+
+    GPIO_Initure.Pin=GPIO_PIN_13;
+    GPIO_Initure.Mode=GPIO_MODE_OUTPUT_PP;
+    GPIO_Initure.Pull=GPIO_PULLUP;
+    GPIO_Initure.Speed=GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOC, &GPIO_Initure);
+
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+    rt_kprintf("Reset the ds18b20!\n");
+    DS18B20_Reset();
+
+    //rt_kprintf("Start to detect the ds18b20!\n"); //DS18B20初始化过程对时间敏感，在初始化过程谨慎使用耗时函数
+    return DS18B20_Check();
+}
+
+static int Ds18b20Init(void)
+{
+    DS18B20_Init();
+    return RT_EOK;
+}\
+MSH_CMD_EXPORT(Ds18b20Init,RT_NULL);
+INIT_APP_EXPORT(Ds18b20Init);
+
+
+
 
